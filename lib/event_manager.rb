2e809efd23b8c8b5214ab738993f90c9173030df
MiKhai37 <mi.khai.thai@proton.me>
2023-12-23 18:24:09 +0100

    refactor zipcode cleaning method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,16 +1,8 @@
 require 'csv'
 
 def clean_zipcode(zipcode)
-  if zipcode.nil?
-    '00000'
-  elsif zipcode.length < 5
-    zipcode.rjust(5, '0')
-  elsif zipcode.length > 5
-    zipcode[0..4]
-  else
-    zipcode
+    zipcode.to_s.rjust(5, '0')[0..4]
   end
-end
 
 puts 'EventManager initialized.'
 
